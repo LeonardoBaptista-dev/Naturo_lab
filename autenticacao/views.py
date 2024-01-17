2d44cdc898fc6cc44996ca8a242624982975cabc
LeonardoBaptista-dev <leonardobaptista.dev@gmail.com>
2024-01-17 09:19:38 -0300

    criando database sqlite

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,7 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .utils import password_is_valid
+from django.contrib.auth.models import User
+
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not password_is_valid (request, senha, confirmar_senha):
+            return redirect('auth/cadastro')
+
+        try:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=senha,
+                                            is_active=False)
+            user.save()
+            return redirect('/auth/logar')
+        except:
+            return redirect('/auth/cadastro')
+        
+                            
 def logar(request):
     return HttpResponse('voce está na pagina de logar')
